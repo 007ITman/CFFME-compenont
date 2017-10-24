35b909ee5660d632d94fc8644c364a00224ef229
zd <1324828410@qq.com>
2017-10-24 22:41:52 +0800

    修改java文件

--- compenont/rep-compenont/src/main/java/com/zd/sources/GitTest.java
+++ compenont/rep-compenont/src/main/java/com/zd/sources/GitTest.java
@@ -2,10 +2,6 @@ package com.zd.sources;
 
 public class GitTest {
 
-	public GitTest() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
