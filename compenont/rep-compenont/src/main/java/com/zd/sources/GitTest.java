4948487b9e5bcec734f3775c4d2ccdffd4d15e7c
zd <1324828410@qq.com>
2017-12-25 21:15:38 +0800

    aaa

--- compenont/rep-compenont/src/main/java/com/zd/sources/GitTest.java
+++ compenont/rep-compenont/src/main/java/com/zd/sources/GitTest.java
@@ -4,12 +4,14 @@ public class GitTest {
 
 	public static void main(String[] args) {
 
-		new GitTest();
+		new GitTest().est();
+	
 
 	}
 	
 	public void est(){
 	
+		System.out.println();
 	}
-
+		
 }
