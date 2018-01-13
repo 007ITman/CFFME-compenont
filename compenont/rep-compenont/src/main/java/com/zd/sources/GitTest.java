4714f87cee905b71fc836e1e42006224d0b28028
zd <1324828410@qq.com>
2018-01-13 10:13:56 +0800

    add a add method

--- compenont/rep-compenont/src/main/java/com/zd/sources/GitTest.java
+++ compenont/rep-compenont/src/main/java/com/zd/sources/GitTest.java
@@ -4,9 +4,14 @@ public class GitTest {
 
 	public static void main(String[] args) {
 
+		GitTest git = new GitTest();
 	}
 	
 	public void test(){
 		System.out.println("branch-compenont-chout86");
+	}
+	
+	public void add(){
+		
 	}
 }
