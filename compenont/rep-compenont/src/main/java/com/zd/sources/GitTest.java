84154bfd4b79b7b105ba0a68c296ec25c7a9f3e2
zd <1324828410@qq.com>
2018-02-08 23:49:30 +0800

    featch

--- compenont/rep-compenont/src/main/java/com/zd/sources/GitTest.java
+++ compenont/rep-compenont/src/main/java/com/zd/sources/GitTest.java
@@ -2,21 +2,4 @@ package com.zd.sources;
 
 public class GitTest {
 
-	public static void main(String[] args) {
-
-		new GitTest().est();
-	
-
-		new GitTest();
-		new GitTest();
-	}
-	
-	public void est(){
-		new GitTest();
-		int a = 0;
-		a = a + 1;
-		new GitTest();
-
-	}
-		
 }
